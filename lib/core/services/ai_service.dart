c96566281172268510ebd944362af905963ec21f
Caio Xavier <proxjiec1@gmail.com>
2026-04-14 09:13:59 -0300

    docs(core): remove model implementation

--- lib/core/services/ai_service.dart
+++ lib/core/services/ai_service.dart
@@ -30,3 +30,5 @@ abstract class AiService {
 // Pseudo-update for commit 18 at 2026-04-06 07:46:43
 
 // Pseudo-update for commit 34 at 2026-04-09 17:13:59
+
+// Pseudo-update for commit 56 at 2026-04-14 09:13:59
